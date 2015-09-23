8c149dda7b3869896148bbb1cb925ec502563c0f
segfault <sf@sharped.net>
2015-09-23 20:22:26 +0200

    Removed useless fields from operations table

--- db/migrate/20150923153135_create_operations.rb
+++ db/migrate/20150923153135_create_operations.rb
@@ -2,9 +2,6 @@ class CreateOperations < ActiveRecord::Migration
   def change
     create_table :operations do |t|
       t.string :operation_type
-      t.datetime :enqueue_date
-      t.datetime :start_date
-      t.datetime :end_time
       t.string :status
 
       t.timestamps null: false
