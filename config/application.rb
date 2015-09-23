20c4fbb3ffdb8f95c4f576d92af06cc2e4732448
segfault <sf@sharped.net>
2015-09-23 18:53:13 +0200

    sidekiq app/workers added to application.rb loaders

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Imagogo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Look for sidekiq workers
+    config.autoload_paths += %W(#{config.root}/app/workers)
   end
 end
