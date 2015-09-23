aedbc9fa1a96feebad426dcbebf3bed8fe8ac2e3
segfault <sf@sharped.net>
2015-09-23 18:46:27 +0200

    Resize image

--- app/workers/op_worker.rb
+++ app/workers/op_worker.rb
@@ -1,5 +1,10 @@
 class OpWorker
   include Sidekiq::Worker
   def resize(image_local, width, height)
+    image = MiniMagick::Image.new(image_local) do |b|
+      b.resize "#{width}x#{height}>"
+      b.format "png"
+      b.write(image_local)
+    end
   end
 end
