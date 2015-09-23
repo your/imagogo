6adf4399324bc4b733bce0202450d13dde6c43be
segfault <sf@sharped.net>
2015-09-23 20:37:53 +0200

    Added play controller & view

---
+++ app/controllers/play_controller.rb
@@ -0,0 +1,11 @@
+class PlayController < ActionController::Base
+  def index
+    @processed_images = ProcessedImage.all
+    @last_operations = Operation.all.limit(10)
+    @processing = Operation.all.where(status: 'running').joins(:image)
+    
+    p @processed_images
+    p @last_operations
+    p @processing
+  end
+end
