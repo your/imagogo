88a3766e882b1b71598af905eb24db38b7c16ee2
segfault <sf@sharped.net>
2015-09-23 17:58:50 +0200

    Adding not null on constraints

--- db/migrate/20150923153430_add_image_to_operations.rb
+++ db/migrate/20150923153430_add_image_to_operations.rb
@@ -1,5 +1,6 @@
 class AddImageToOperations < ActiveRecord::Migration
   def change
     add_reference :operations, :image, index: true, foreign_key: true
+    change_column_null :operations, :image, false
   end
 end
--- db/migrate/20150923153542_add_image_to_processed_images.rb
+++ db/migrate/20150923153542_add_image_to_processed_images.rb
@@ -1,5 +1,6 @@
 class AddImageToProcessedImages < ActiveRecord::Migration
   def change
     add_reference :processed_images, :image, index: true, foreign_key: true
+    change_column_null :processed_images, :image, false
   end
 end
