e6e076dc325711afd79678c6ac6dec2191630fbe
segfault <sf@sharped.net>
2015-09-25 12:14:20 +0200

    add js polling on upload status (more accurate)

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,10 +1,15 @@
 function pollIt(id) {
 	$('#loader-'+id).show()
 	$.poll(function(retry) {
-		$.get('/op/'+id, function(response, status) {
+		$.get('/op/'+id+'.json', function(response, status) {
 			if (status == 'success') {
-				$('#loader-'+id).hide();
-				$('#img-'+id).show();
+				op_type = response["operation_type"];
+				op_status = response["status"];
+				if (op_type === "resize" && op_status === "ok") {
+					$('#loader-'+id).hide();
+					$('#img-'+id).show();
+				} else
+					retry()
 			} else
 				retry()
 		})
